266c5caaa93f6dff0795fb6f3eef0111841e1cc2
Jeffrey Ilar <jilar@petzent.com>
2019-10-04 15:51:53 -0700

    Refactored app component into class component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
-  state = {term: ''}
+  state = {term: ''};
+
+  onFormSubmit= (event) => {
+    event.preventDefault();
+    //props in class components are preceded by this. 
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className= "ui segment">
-        <form className ="ui form">
+        <form onSubmit={this.onFormSubmit} className ="ui form">
           <div className= "field">
             <label>Image Search</label>
             <input
               type= "text"
-              value={this.state.term} 
+              value={this.state.term}
               onChange={(e) => this.setState({term:e.target.value})}
             />
           </div>
