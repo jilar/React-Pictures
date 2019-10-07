67fde9f4b3d860c3dae455d28e531745ee129996
Jeffrey Ilar <jilar@petzent.com>
2019-10-07 11:48:52 -0700

    Created Api Request to unsplashed API with axios; markup for ImageList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar';
+import ImageList from './ImageList'
 
 class App extends React.Component{
-  onSearchSubmit(term){
-    console.log(term);
+  state={images: []};
+
+   //async onSearchSubmit=(term){
+   //refactored to fix .this error
+   onSearchSubmit=async (term) => {
+    const response = await unsplash.get('/search/photos',{
+       params:{ query: term },
+     });
+
+     this.setState({images: response.data.results});
+     //alternative way to write promise statement without async-await
+     //.then((response) =>{
+     //console.log(response.data.results);
+     //})
   }
+
 
   render(){
     return(
       <div className="ui container" style ={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     );
   }
